be930737c4ce4edbc99b753f117d11d417461449
uprel <uros00@gmail.com>
2017-12-04 09:08:58 +0100

    Norwegian translate strings

--- application/language/norwegian/gisportal_lang.php
+++ application/language/norwegian/gisportal_lang.php
@@ -1,54 +1,50 @@
 <?php
 
-$lang['gp_welcome_title'] = 'Welcome!';
-$lang['gp_welcome_message'] = 'Please login to browse projects...';
+$lang['gp_welcome_title'] = 'Velkommen!';
+$lang['gp_welcome_message'] = 'Logg inn for å få tilgang...';
 
-$lang['gp_login'] = 'Login';
-$lang['gp_register'] = 'Sign Up';
-$lang['gp_register_success'] = 'You are Successfully Registered! Please login to continue!';
-$lang['gp_register_error'] = 'Oops! Error.  Please try again later!!!';
-$lang['gp_log_out'] = 'Log out';
-$lang['gp_user'] = 'User';
-$lang['gp_password'] = 'Password';
-$lang['gp_cancel'] = 'Cancel';
-$lang['gp_login_wrong'] = 'Wrong Username or password!';
-$lang['gp_new_user'] = 'New User?';
-$lang['gp_already_registered'] = 'Already Registered?';
-$lang['gp_first_name'] = 'First Name';
-$lang['gp_last_name'] = 'Last Name';
-$lang['gp_email'] = 'E-mail';
-$lang['gp_username'] = 'Username';
-$lang['gp_username_placeholder'] = 'Username or Email';
-$lang['gp_confirm'] = 'Confirm';
+$lang['gp_login'] = 'Logg inn';
+$lang['gp_register'] = 'Registrer';
+$lang['gp_register_success'] = 'Registrert! Logg inn for å fortsette!';
+$lang['gp_register_error'] = 'Oops! Noe gikk galt, prøv igjen senere!!!';
+$lang['gp_log_out'] = 'Logg ut';
+$lang['gp_user'] = 'Brukernavn';
+$lang['gp_password'] = 'Passord';
+$lang['gp_cancel'] = 'Kanseler';
+$lang['gp_login_wrong'] = 'Feil brukernavn eller passord!';
+$lang['gp_new_user'] = 'Ny bruker?';
+$lang['gp_already_registered'] = 'Allerede registrert?';
+$lang['gp_first_name'] = 'Fornavn';
+$lang['gp_last_name'] = 'Etternavn';
+$lang['gp_email'] = 'Epost';
+$lang['gp_username'] = 'Brukernavn';
+$lang['gp_username_placeholder'] = 'Brukernavn eller epost';
+$lang['gp_confirm'] = 'Forsett?';
 $lang['gp_here'] = 'here';
 
-$lang['gp_view_projects'] = 'View projects';
-$lang['gp_open_project'] = 'Open project';
+$lang['gp_view_projects'] = 'Se prosjekter';
+$lang['gp_open_project'] = 'åpne prosjekt';
 
-$lang['gp_profile_title'] = 'My profile';
-$lang['gp_clients_title'] = 'Clients';
-$lang['gp_projects_title'] = 'Projects';
-$lang['gp_home'] = 'Home';
-$lang['gp_portal_title'] = 'GIS PORTAL';
+$lang['gp_profile_title'] = 'Min profil';
+$lang['gp_clients_title'] = 'Klienter';
+$lang['gp_projects_title'] = 'Prosjekter';
+$lang['gp_home'] = 'Hjem';
+$lang['gp_portal_title'] = 'Geonord GIS portal';
 
-$lang['gp_user_projects'] = 'User projects';
-$lang['gp_public_projects'] = 'Public projects';
-$lang['gp_all_projects'] = 'All projects';
-$lang['gp_client'] = 'client';
-$lang['gp_project'] = 'project';
-$lang['gp_public'] = 'public';
-$lang['gp_crs'] = 'crs';
-$lang['gp_contact'] = 'contact';
-$lang['gp_description'] = 'description';
+$lang['gp_user_projects'] = 'Bruker prosjekt';
+$lang['gp_public_projects'] = 'Offentlige prosjekter';
+$lang['gp_all_projects'] = 'Alle Prosjekter';
+$lang['gp_client'] = 'Klient';
+$lang['gp_project'] = 'Prosjekt';
+$lang['gp_public'] = 'Offentlig';
+$lang['gp_crs'] = 'CRS';
+$lang['gp_contact'] = 'Kontakt';
+$lang['gp_description'] = 'Forklaring';
 
-$lang['gp_registered'] = 'Registered';
-$lang['gp_last_login'] = 'Last login';
-$lang['gp_count_login'] = 'Count login';
-$lang['gp_language'] = 'Language';
+$lang['gp_registered'] = 'Registrert';
+$lang['gp_last_login'] = 'Forrige innlogging';
+$lang['gp_count_login'] = 'Antall innlogginger';
+$lang['gp_language'] = 'Språk';
 
-$lang['gp_no_projects'] = 'Currently no private projects for you!';
-$lang['gp_no_public_projects'] = 'Currently no public projects to browse!';
-
-
-
-
+$lang['gp_no_projects'] = 'Ingen private prosjekter!';
+$lang['gp_no_public_projects'] = 'Ingen offentlige prosjekter!';

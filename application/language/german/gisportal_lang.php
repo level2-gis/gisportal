ae350f96df399c4d157d952e0af09777ac3c0cc5
uprel <uros00@gmail.com>
2018-01-05 07:49:35 +0100

    German translation

---
+++ application/language/german/gisportal_lang.php
@@ -0,0 +1,54 @@
+<?php
+
+$lang['gp_welcome_title'] = 'Hallo!';
+$lang['gp_welcome_message'] = 'Bitte einloggen um Deine verfügbaren Projekte zu sehen...';
+
+$lang['gp_login'] = 'Login';
+$lang['gp_register'] = 'Registrierung';
+$lang['gp_register_success'] = 'Du hast die Registrierung erfolgreich abgeschlossen, bitte melde Dich erneut an!';
+$lang['gp_register_error'] = 'Hoppla! Fehler.  Bitte nochmal versuchen!!!';
+$lang['gp_log_out'] = 'Abmelden';
+$lang['gp_user'] = 'Benutzer';
+$lang['gp_password'] = 'Passwort';
+$lang['gp_cancel'] = 'Abbruch!';
+$lang['gp_login_wrong'] = 'Falscher Benutzer oder Passwort!';
+$lang['gp_new_user'] = 'Neuer Benutzer?';
+$lang['gp_already_registered'] = 'Bereits Registriert?';
+$lang['gp_first_name'] = 'Vorname';
+$lang['gp_last_name'] = 'Nachname';
+$lang['gp_email'] = 'E-mail';
+$lang['gp_username'] = 'Benutzer';
+$lang['gp_username_placeholder'] = 'Benutzername oder Email';
+$lang['gp_confirm'] = 'bestätige';
+$lang['gp_here'] = 'hier!';
+
+$lang['gp_view_projects'] = 'Projekte anzeigen';
+$lang['gp_open_project'] = 'Projekte öffnen';
+
+$lang['gp_profile_title'] = 'Mein Profil';
+$lang['gp_clients_title'] = 'Clients';
+$lang['gp_projects_title'] = 'Projekte';
+$lang['gp_home'] = 'Home';
+$lang['gp_portal_title'] = 'GIS PORTAL';
+
+$lang['gp_user_projects'] = 'Deine Projekte';
+$lang['gp_public_projects'] = 'öffentliche Projekte';
+$lang['gp_all_projects'] = 'alle Projekte';
+$lang['gp_client'] = 'Client';
+$lang['gp_project'] = 'Projekt';
+$lang['gp_public'] = 'öffentlich';
+$lang['gp_crs'] = 'Koordinatensystem';
+$lang['gp_contact'] = 'Kontakt';
+$lang['gp_description'] = 'Beschreibung';
+
+$lang['gp_registered'] = 'Registriert';
+$lang['gp_last_login'] = 'Letzter Login';
+$lang['gp_count_login'] = 'Login Zähler';
+$lang['gp_language'] = 'Sprache';
+
+$lang['gp_no_projects'] = 'Momentan sind keine Projekte für Dich freigegeben!';
+$lang['gp_no_public_projects'] = 'Momentan sind keine öffentlichen Projekte verfügbar!';
+
+
+
+

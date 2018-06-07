a9004e2677a2a3bb52b208f490c074ec98431806
uprel <uros00@gmail.com>
2018-06-07 06:29:34 +0200

    QGS mime type

--- application/config/mimes.php
+++ application/config/mimes.php
@@ -179,5 +179,6 @@ return array(
 	'odt'	=>	'application/vnd.oasis.opendocument.text',
 	'odm'	=>	'application/vnd.oasis.opendocument.text-master',
 	'ott'	=>	'application/vnd.oasis.opendocument.text-template',
-	'oth'	=>	'application/vnd.oasis.opendocument.text-web'
+	'oth'	=>	'application/vnd.oasis.opendocument.text-web',
+    'qgs'   =>  'text/html'
 );

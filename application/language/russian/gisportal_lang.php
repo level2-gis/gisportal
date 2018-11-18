ecfc952895fb0b6f27683536588dd0a3d488d3ce
uprel <uros00@gmail.com>
2018-11-18 18:15:58 +0100

    Russian translation

---
+++ application/language/russian/gisportal_lang.php
@@ -0,0 +1,113 @@
+<?php
+
+$lang['gp_welcome_title'] = 'Добро пожаловать!';
+$lang['gp_welcome_message'] = 'Выполните вход, чтобы увидеть проекты...';
+
+$lang['gp_login'] = 'Вход';
+$lang['gp_register'] = 'Зарегистрироваться';
+$lang['gp_register_success'] = 'Регистрация завершена. Выполните вход, чтобы продолжить.';
+$lang['gp_register_error'] = 'Ошибка. Попробуйте позже.';
+$lang['gp_log_out'] = 'Выход';
+$lang['gp_user'] = 'Имя пользователя';
+$lang['gp_password'] = 'Пароль';
+$lang['gp_cancel'] = 'Отмена';
+$lang['gp_login_wrong'] = 'Неверное имя пользователя или пароль!';
+$lang['gp_new_user'] = 'Новый пользователь?';
+$lang['gp_already_registered'] = 'Уже зарегистрированы?';
+$lang['gp_first_name'] = 'Имя';
+$lang['gp_last_name'] = 'Фамилия';
+$lang['gp_email'] = 'E-mail';
+$lang['gp_username'] = 'Имя пользователя';
+$lang['gp_organization'] = 'Организация';
+$lang['gp_username_placeholder'] = 'Имя пользователя';
+$lang['gp_confirm'] = 'Подтвердить';
+$lang['gp_here'] = 'здесь';
+
+$lang['gp_view_projects'] = 'Просмотреть проекты';
+$lang['gp_open_project'] = 'Открыть проект';
+
+$lang['gp_profile_title'] = 'Мой профиль';
+$lang['gp_clients_title'] = 'Клиенты';
+$lang['gp_projects_title'] = 'Проекты';
+$lang['gp_layers_title'] = 'Слои';
+$lang['gp_users_title'] = 'Пользователи';
+$lang['gp_home'] = 'Главная';
+$lang['gp_portal_title'] = 'Портал ГИС';
+
+$lang['gp_user_projects'] = 'Проекты пользователя';
+$lang['gp_public_projects'] = 'Public projects';
+$lang['gp_all_projects'] = 'Все проекты';
+$lang['gp_client'] = 'Клиент';
+$lang['gp_project'] = 'Проект';
+$lang['gp_layer'] = 'Слой';
+$lang['gp_public'] = 'public';
+$lang['gp_crs'] = 'Система координат';
+$lang['gp_contact'] = 'Контакты';
+$lang['gp_description'] = 'Описание';
+
+$lang['gp_registered'] = 'Дата регистрации';
+$lang['gp_last_login'] = 'Последняя авторизация';
+$lang['gp_count_login'] = 'Количество авторизаций';
+$lang['gp_language'] = 'Язык';
+
+$lang['gp_no_projects'] = 'Отсутствуют права на редактирование какого-либо проекта.';
+$lang['gp_no_public_projects'] = 'Currently no public projects to browse!';
+
+$lang['gp_uploaded_files'] = 'Uploaded files';
+$lang['gp_upload_no_folder'] = 'Please create main upload folder on disk, defined in config.php: ';
+$lang['gp_upload_success'] = 'Your file was successfully uploaded!';
+
+$lang['gp_edit'] = 'Редактировать';
+$lang['gp_delete'] = 'Удалить';
+$lang['gp_create'] = 'Создать';
+$lang['gp_new'] = 'Новый';
+$lang['gp_saved'] = ' has been saved';
+$lang['gp_deleted'] = ' has been deleted';
+$lang['gp_exists'] = ' already exists';
+
+//database fields
+$lang['gp_name'] = 'Название';
+$lang['gp_display_name'] = 'Отображаемое название';
+$lang['gp_order'] = 'Display Order';
+$lang['gp_url'] = 'Веб-сайт';
+$lang['gp_new_client'] = 'Новый клиент';
+$lang['gp_name_tip'] = 'This is QGIS Project file name without .qgs!';
+$lang['gp_size'] = 'Размер';
+$lang['gp_date'] = 'Дата';
+$lang['gp_qgis_project'] = 'Проект QGIS';
+$lang['gp_display_name_tip'] = 'This is set from QGIS Project properties!';
+$lang['gp_select_client'] = 'Select Client';
+$lang['gp_select_layer'] = 'Select Layer';
+$lang['gp_new_layer'] = 'Новый слой';
+$lang['gp_new_project'] = 'Новый проект';
+$lang['gp_project_count'] = 'Количество проектов';
+$lang['gp_action'] = 'Действие';
+$lang['gp_image'] = 'Изображение';
+$lang['gp_save'] = 'Сохранить';
+$lang['gp_return'] = 'Возврат';
+$lang['gp_type'] = 'Тип';
+$lang['gp_definition'] = 'Определение';
+$lang['gp_properties'] = 'Свойства';
+$lang['gp_base_layers'] = 'Геоподложки';
+$lang['gp_overlay_layers'] = 'Overlay-слои';
+$lang['gp_overview_layer'] = 'Overview layer';
+$lang['gp_feedback_email'] = 'Feedback email';
+$lang['gp_geolocation'] = 'Geolocation';
+$lang['gp_restrict'] = 'Restrict to start extent';
+$lang['gp_feedback'] = 'Обратная связь';
+$lang['gp_measurements'] = 'Measurements';
+$lang['gp_print'] = 'Печать';
+$lang['gp_zoom_back_forward'] = 'Zoom back/forward';
+$lang['gp_identify_mode'] = 'Identify mode';
+$lang['gp_permalink'] = 'Permalink';
+$lang['gp_available'] = 'Зарегистрированные';
+$lang['gp_in_project'] = 'в проекте';
+$lang['gp_access'] = 'Доступ';
+$lang['gp_make_admin'] = 'Сделать пользователя администратором';
+$lang['gp_user_is_admin'] = 'User is admin and has access to all projects!';
+$lang['gp_code'] = 'Code';
+$lang['gp_add_new_file'] = 'Добавить новый файл';
+$lang['gp_upload'] = 'Загрузить';
+$lang['gp_info_demo'] = 'This is now only demonstration for each client web repository for user uploaded files.
+        Files uploaded from projects are in projects subfolders.';
+$lang['gp_plugins'] = 'Плагины';

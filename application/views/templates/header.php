271f95428f7fffb9026bc9aa30d95bf82088ea59
Uroš <uros00@gmail.com>
2019-05-18 09:09:47 +0200

    No header menu on login page

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -91,6 +91,7 @@
 <body>
 
 <!-- Navigation -->
+<?php if ($logged_in) : ?>
 <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
     <div class="container">
         <!-- Brand and toggle get grouped for better mobile display -->
@@ -109,19 +110,17 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="navbar1">
             <ul class="nav navbar-nav navbar-right">
-                <?php if ($logged_in){ ?>
-
-                    <?php if ($is_admin){ ?>
+                    <?php if ($is_admin) : ?>
                         <li><a href="<?php echo site_url('/clients'); ?>"><i class="fa fa-folder"></i> <span><?php echo $this->lang->line('gp_clients_title'); ?></span></a></li>
                         <li><a href="<?php echo site_url('/project_groups'); ?>"><i class="fa fa-list"></i> <span><?php echo $this->lang->line('gp_groups_title'); ?></span></a></li>
-                    <?php } ?>
+                    <?php endif; ?>
 
                     <li><a href="<?php echo site_url('/projects'); ?>"><i class="fa fa-file-text"></i> <span><?php echo $this->lang->line('gp_projects_title'); ?></span></a></li>
 
-                    <?php if ($is_admin){ ?>
+                    <?php if ($is_admin) : ?>
                         <li><a href="<?php echo site_url('/layers'); ?>"><i class="fa fa-database"></i> <span><?php echo $this->lang->line('gp_layers_title'); ?></span></a></li>
                         <li><a href="<?php echo site_url('/users'); ?>"><i class="fa fa-group"></i> <span><?php echo $this->lang->line('gp_users_title'); ?></span></a></li>
-                    <?php } ?>
+                    <?php endif; ?>
 
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> <?php echo $this->session->userdata('user_name'); ?> <span class="caret"></span></a>
@@ -130,14 +129,11 @@
                             <li><a href="<?php echo site_url('/auth/logout') ?>"><?php echo $this->lang->line('gp_log_out'); ?></a></li>
                         </ul>
                     </li>
-                <?php } else { ?>
-                    <li><a href="<?php echo site_url('/auth/login') ?>"><?php echo $this->lang->line('gp_login'); ?></a></li>
-                    <li><a href="<?php echo site_url('/signup') ?>"><?php echo $this->lang->line('gp_register'); ?></a></li>
-                <?php } ?>
             </ul>
         </div>
         <!-- /.navbar-collapse -->
     </div>
     <!-- /.container -->
 </nav>
+<?php endif; ?>
 <div class="container body-content">

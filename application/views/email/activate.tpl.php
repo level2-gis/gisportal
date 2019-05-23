731d8e62ed0b1f3ac76ad5dd5f4260be480d3567
Uroš <uros00@gmail.com>
2019-05-23 06:51:22 +0200

    Add footer to email templates

--- application/views/email/activate.tpl.php
+++ application/views/email/activate.tpl.php
@@ -2,5 +2,6 @@
 <body>
 	<h3><?php echo sprintf(lang('email_activate_heading'), $identity);?></h3>
 	<p><?php echo sprintf(lang('email_activate_subheading'), anchor('auth/activate/'. $id .'/'. $activation, lang('email_activate_link')));?></p>
+    <footer><?php echo $this->config->item('site_title') . ' - ' . $_SERVER['SERVER_NAME']; ?></footer>
 </body>
 </html>
--- application/views/email/forgot_password.tpl.php
+++ application/views/email/forgot_password.tpl.php
@@ -2,5 +2,6 @@
 <body>
 	<h3><?php echo sprintf(lang('email_forgot_password_heading'), $identity);?></h3>
 	<p><?php echo sprintf(lang('email_forgot_password_subheading'), anchor('auth/reset_password/'. $forgotten_password_code, lang('email_forgot_password_link')));?></p>
+    <footer><?php echo $this->config->item('site_title') . ' - ' . $_SERVER['SERVER_NAME']; ?></footer>
 </body>
 </html>

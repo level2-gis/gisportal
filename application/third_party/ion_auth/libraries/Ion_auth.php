5a1d27ac52a5db77fc30fb8d7baa006327cecb79
Uroš <uros00@gmail.com>
2019-09-16 10:06:34 +0200

    Send group emails in bcc (hidden)

--- application/third_party/ion_auth/libraries/Ion_auth.php
+++ application/third_party/ion_auth/libraries/Ion_auth.php
@@ -453,7 +453,8 @@ class Ion_auth
 
         $this->email->clear();
         $this->email->from($this->config->item('admin_email'), $this->config->item('site_title'));
-        $this->email->to(empty($to) ? $this->config->item('admin_email') : $to);
+        //if want to show emails, just use to, by default is bcc and limit parameter which splits into more batches if emails>limit
+        $this->email->bcc(empty($to) ? $this->config->item('admin_email') : $to, 100);
         $this->email->subject($this->config->item('site_title') . ' - ' . $subject);
         $this->email->message($message);
 

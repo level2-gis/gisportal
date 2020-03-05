45a0805e2cb15c120b73ed44704d1352647dfc79
uros <uros00@gmail.com>
2020-03-05 09:34:31 +0100

    update footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,6 +1,6 @@
 <footer class="footer">
     <div class="container text-center">
-        <p style="-webkit-text-fill-color: grey">&copy; <a href="<?php echo $this->config->item('company_url'); ?>" target="_blank"><?php echo $this->config->item('company'); ?></a> 2019, Created with <a href="https://github.com/uprel/gisportal" target="_blank">GisPortal!</a></p>
+        <p style="-webkit-text-fill-color: grey">&copy; <a href="<?php echo $this->config->item('company_url'); ?>" target="_blank"><?php echo $this->config->item('company'); ?></a> <?php echo date("Y"); ?>, Created with <a href="https://github.com/uprel/gisportal" target="_blank">GisPortal!</a></p>
     </div>
 </footer>
 </body>

a1a36f02712b704391de8e41a32966892dc76f1c
Uroš <uros00@gmail.com>
2020-04-16 09:03:04 +0200

    footer fix

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,7 +1,11 @@
-<footer class="footer">
-    <div class="container text-center">
-        <p style="-webkit-text-fill-color: grey">&copy; <a href="<?php echo $this->config->item('company_url'); ?>" target="_blank"><?php echo $this->config->item('company'); ?></a> <?php echo date("Y"); ?>, Created with <a href="https://github.com/uprel/gisportal" target="_blank">GisPortal!</a></p>
-    </div>
-</footer>
+<div class="container body-content">
+	<footer class="footer">
+		<div class="container text-center">
+			<p style="-webkit-text-fill-color: grey">&copy; <a href="<?php echo $this->config->item('company_url'); ?>"
+															   target="_blank"><?php echo $this->config->item('company'); ?></a> <?php echo date("Y"); ?>
+				, Created with <a href="https://github.com/uprel/gisportal" target="_blank">GisPortal!</a></p>
+		</div>
+	</footer>
+</div>
 </body>
 </html>

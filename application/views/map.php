fa28117de665a8ce0e58163aec5b747d9991b567
uros <uros00@gmail.com>
2020-08-14 12:57:43 +0200

    trigger js version

--- application/views/map.php
+++ application/views/map.php
@@ -12,4 +12,4 @@
 <div id="map" class="map"></div>
 <div class="help-block" id="projection"></div>
 <div id="mouse-position"></div>
-<script type="text/javascript" src="<?php echo base_url("assets/js/gisportal_map.js?v=20200705"); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url("assets/js/gisportal_map.js?v=20200813"); ?>"></script>

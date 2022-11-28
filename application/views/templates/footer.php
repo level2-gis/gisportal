bb271957d16e2a592f81d217806686c836249efc
Uroš Preložnik <uros00@gmail.com>
2022-11-28 12:01:21 +0100

    footer update

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,18 +1,23 @@
 <div class="container body-content">
-<footer class="footer">
-    <div class="container text-center">
-           <ul class="list-inline grey">
+	<footer class="footer">
+		<div class="container text-center">
+			<ul class="list-inline grey">
 
-                    <li>
-                        <a class="grey" href="https://site.geo-portal.si/index.html"  target="_blank">Predstavitev</a>
-                    </li>
-                  	<li>|</li>
-                    <li>
-                        &copy; <a class="grey" href="<?php echo $this->config->item('company_url'); ?>" target="_blank"><?php echo $this->config->item('company'); ?></a> <?php echo date("Y"); ?>
-                    </li>
-                </ul>
-	</div>
-</footer>
+				<li>
+					<a class="grey" href="https://site.geo-portal.si/index.html" target="_blank">Predstavitev</a>
+				</li>
+				<li>|</li>
+				<li>
+					<a class="grey" href="https://site.geo-portal.si/pogoji-uporabe.html" target="_blank">Pogoji uporabe</a>
+				</li>
+				<li>|</li>
+				<li>
+					&copy; <a class="grey" href="<?php echo $this->config->item('company_url'); ?>"
+							  target="_blank"><?php echo $this->config->item('company'); ?></a> <?php echo date("Y"); ?>
+				</li>
+			</ul>
+		</div>
+	</footer>
 </div>
 </body>
 </html>

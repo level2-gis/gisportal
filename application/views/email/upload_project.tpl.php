6211e218408dd37089ca9e60c3588d18c50ec8c5
Uroš Preložnik <uros00@gmail.com>
2024-07-04 08:27:03 +0200

    upload project email change

--- application/views/email/upload_project.tpl.php
+++ application/views/email/upload_project.tpl.php
@@ -1,8 +1,8 @@
 <html>
 <body>
-<h3>Nova verzija projekta <?php echo $file_name; ?></h3>
-<p><?php echo $client['display_name']; ?></p>
+<h3><?php echo $file_name; ?></h3>
 <p>Uporabnik <?php echo $user['display_name']; ?></p>
+<p><?php echo $client['display_name']; ?></p>
 <br><br>
 <hr>
 <footer><?php echo anchor(site_url(), $this->config->item('site_title') . ' - ' . $_SERVER['SERVER_NAME']); ?></footer>

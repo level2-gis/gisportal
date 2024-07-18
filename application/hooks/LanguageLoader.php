3643e6c84750c15a4cb6d2ce862e88f9e46b2a5c
uros <uros00@gmail.com>
2024-07-18 12:36:11 +0200

    add username to apache log for statistics

--- application/hooks/LanguageLoader.php
+++ application/hooks/LanguageLoader.php
@@ -8,6 +8,7 @@ class LanguageLoader
         $defLang = $ci->config->item('language');
 
         $siteCode = $ci->session->userdata('lang');
+		$user = $ci->session->userdata('user_name');
         if ($siteCode) {
             $siteLang = get_language_name($siteCode);
             $ci->lang->load('auth', $siteLang);
@@ -20,5 +21,8 @@ class LanguageLoader
             $ci->lang->load('ion_auth', $defLang);
             $ci->lang->load('gisportal', $defLang);
         }
+        if ($user) {
+			apache_note('username',$user);
+		}
     }
 }

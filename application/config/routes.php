ff0564359d20e9e8aa288bf8609bff5250be389c
Uroš Preložnik <uros00@gmail.com>
2026-07-08 13:42:40 +0200

    routes for level2 api

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,3 +52,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'home';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+$route['modules/level2/proxy'] = 'modules/level2/proxy';
+$route['modules/level2/proxy/(.*)'] = 'modules/level2/proxy/$1';
